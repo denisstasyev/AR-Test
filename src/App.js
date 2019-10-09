457da12de13230fe67a61293175cf88623f4982e
Denis Stasyev <denisstasyev@gmail.com>
2019-10-09 12:16:45 +0300

    Fix AR content

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
             <div className="App">
                 <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
                     <a-anchor hit-testing-enabled="true">
-                        <a-box position='0 0 5' material='opacity: 0.5;  color: yellow'></a-box>
+                        <a-box position='0 0 0' material='opacity: 0.5;  color: yellow'></a-box>
                     </a-anchor>                    
                     <a-camera-static preset="hiro" />
                 </Scene>
