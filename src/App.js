aa16428e6b7d0d00cfaa43d0c03b0413a9e5623f
Denis Stasyev <denisstasyev@gmail.com>
2019-10-09 20:08:48 +0300

    Add custom model

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
                 <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
                     <a-marker preset="hiro">
                         {/*<a-box position='0 0 0' material='opacity: 0.5; color: yellow;'></a-box>*/}
-                        <a-entity position="0 0 0"
+                        <a-entity rotation="180 0 0" position="10 0 0"
                             obj-model="obj: url(models/dog.obj); mtl: url(models/dog.mtl)"></a-entity>
                     </a-marker>
                     <a-entity camera />
