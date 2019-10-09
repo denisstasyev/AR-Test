4cc15e358d8e0a874679dda244129ee1ddc3e019
Denis Stasyev <denisstasyev@gmail.com>
2019-10-09 11:51:43 +0300

    Fix AR content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import "aframe";
-import { Scene } from 'aframe-react';
+import { Scene, Entity } from 'aframe-react';
  
 class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <Scene artoolkit={{sourceType: 'webcam'}}>
- 
+                    <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
                     
                     <a-box position='0 0 0' material='opacity: 0.5; color: yellow'></a-box>
                     
