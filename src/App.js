b283fa20d7bbb2eb0ac4feb739d4ea022a305614
Denis Stasyev <denisstasyev@gmail.com>
2019-10-09 12:32:05 +0300

    Change AR content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     render() {
         return (
             //<div className="App">
-<a-scene embedded arjs>
+<a-scene embedded arjs='sourceType: webcam;'>
     <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
     <a-marker-camera preset='hiro'></a-marker-camera>
   </a-scene>
