9bff585bc58d9669a1382e9ba930f5f04d1318a3
Denis Stasyev <denisstasyev@gmail.com>
2019-10-09 13:47:05 +0300

    Change AR content back

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import './App.css';
 
-//import { Scene } from 'aframe-react';
+import { Scene } from 'aframe-react';
  
 class App extends React.Component {
     render() {
         return (
-            //<div className="App">
-<a-scene embedded arjs='sourceType: webcam;'>
-    <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-    <a-marker-camera preset='hiro'></a-marker-camera>
-  </a-scene>
-                //<Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
-                //    <a-anchor hit-testing-enabled="true">
-                //        <a-box position='0 0 0' material='opacity: 0.5;  color: yellow'></a-box>
-                //    </a-anchor>                    
-                //    <a-camera-static preset="hiro" />
-                //</Scene>
-            //</div>
+            <div className="App">
+                <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
+
+                    <a-anchor hit-testing-enabled="true">
+                        <a-box position='0 0 -5' material='opacity: 0.5;  color: yellow'></a-box>
+                    </a-anchor>                    
+                    <a-camera-static preset="hiro" />
+                </Scene>
+            </div>
         );
     }
 }
