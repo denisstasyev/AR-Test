fc0c7c15671c05b75537da53c8a9f6dbe4e69097
Denis Stasyev <denisstasyev@gmail.com>
2019-10-09 11:11:14 +0300

    Fix AR content

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 
 // import "aframe";
-//import { Scene } from 'aframe-react';
+import { Scene } from 'aframe-react';
  
 class App extends React.Component {
     render() {
         return (
-  <a-scene embedded arjs>
-    <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-    <a-marker-camera preset='hiro'></a-marker-camera>
-  </a-scene>
+            <div className="App">
+                <Scene artoolkit={{sourceType: 'webcam'}}>
+ 
+                    <a-anchor hit-testing-enabled="false">
+                        <a-box position='0 0 0' material='opacity: 0.5; color: yellow'></a-box>
+                    </a-anchor>
+                    <a-camera-static preset="hiro" />
+                </Scene>
+            </div>
         );
     }
 }
