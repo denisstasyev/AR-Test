4f5e5d5c35c0e08fc6c80f63700a93ed07fd839d
Denis Stasyev <denisstasyev@gmail.com>
2019-10-09 16:21:37 +0300

    Attempt with raw AR.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-var thisIsMyCopy = '<a-scene embedded arjs>\
-      <a-marker preset="hiro">\
-          <a-box position="0 0.5 0" material="color: yellow;"></a-box>\
-      </a-marker>\
-      <a-entity camera></a-entity>\
-    </a-scene>'
+var thisIsMyCopy = `<a-scene embedded arjs>
+      <a-marker preset="hiro">
+          <a-box position="0 0.5 0" material="color: yellow;"></a-box>
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>`;
 
 function App() {
   return (
