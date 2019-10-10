849e4f3fbe2db45b95c29e709cce83d4367dbca7
Denis Stasyev <denisstasyev@gmail.com>
2019-10-10 19:04:57 +0300

    Change implementing idea

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './App.css';
 
-import { Scene } from 'aframe-react';
+//import { Scene } from 'aframe-react';
  
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
+<React.Fragment>
                     <a-marker preset="hiro">
                         {/*<a-box position='0 0 0' material='opacity: 0.5; color: yellow;'></a-box>*/}
                         <a-entity rotation="0 180 0" position="100 0 0"
                             obj-model="obj: url(models/dog.obj); mtl: url(models/dog.mtl)"></a-entity>
                     </a-marker>
                     <a-entity camera />
-                </Scene>
-            </div>
+                
+</React.Fragment>
         );
     }
 }
