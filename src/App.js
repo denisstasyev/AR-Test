681b9a5b41ab4443b21a50f9e744fcb418cd8489
Denis Stasyev <denisstasyev@gmail.com>
2019-10-12 23:19:55 +0300

    Add custom 3D model with nice dog, not cat :)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
             <div className="App">
                 <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}> {/* debugUIEnabled: 'false' - not working here */}
                     <a-anchor hit-testing-enabled="true">
-                        <a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>
+                        <a-entity position='0 0 0' scale='0.05 0.05 0.05' rotation='200 0 0' obj-model='obj: url(models/dog.obj); mtl: url(models/dog.mtl)'></a-entity>
+                        {/*<a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>*/}
                     </a-anchor>
                     <a-camera-static preset="hiro" />
                 </Scene>
