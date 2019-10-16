f1bc40e8b7ab11e9d66f3ecf7ed4a77bc5fe9b53
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-16 15:15:44 +0000

    Deploy denisstasyev/AR-Test to github.com/denisstasyev/AR-Test.git:gh-pages

---
+++ precache-manifest.f95f4d52ded4e2eff71952649a4516c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0022cf4e7b3c00071053fd288bb38cef",
+    "url": "/AR-Test/index.html"
+  },
+  {
+    "revision": "9e17e313c09b9514c261",
+    "url": "/AR-Test/static/css/main.6d502f0b.chunk.css"
+  },
+  {
+    "revision": "2c78b4e7ff3724b33dfc",
+    "url": "/AR-Test/static/js/2.31b4e2d1.chunk.js"
+  },
+  {
+    "revision": "9e17e313c09b9514c261",
+    "url": "/AR-Test/static/js/main.24813a20.chunk.js"
+  },
+  {
+    "revision": "3866847a17f8843fdbc8",
+    "url": "/AR-Test/static/js/runtime-main.6fc07cbf.js"
+  }
+]);
